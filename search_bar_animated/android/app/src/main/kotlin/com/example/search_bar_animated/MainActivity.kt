4782b1a1c0d51be36249ab8a3c30308dc03fb805
Hasnain-Mirrani07 <115531664+Hasnain-Mirrani07@users.noreply.github.com>
2023-09-18 12:18:46 +0500

    animated_Search_Bar_flutter

---
+++ search_bar_animated/android/app/src/main/kotlin/com/example/search_bar_animated/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.search_bar_animated
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ search_bar_animated/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'animated_search_bar.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Anim search bar Example',
+      home: App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+          child: AnimSearchBar(
+            width: 400,
+            textController: textController,
+            onSuffixTap: () {
+              setState(() {
+                textController.clear();
+              });
+            },
+            rtl: false,
+            onSubmitted: (String value) {
+              debugPrint("onSubmitted value: " + value);
+            },
+            textInputAction: TextInputAction.search,
+            searchBarOpen: (a) {
+              a = 0;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
